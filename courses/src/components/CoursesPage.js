1c8616d65ddf29470348a029f5436247c960b710
Olga <olganedelcuam@gmail.com>
2023-01-22 17:35:44 +0100

    fetching the courses from the CareerFoundry API

--- courses/src/components/CoursesPage.js
+++ courses/src/components/CoursesPage.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const CoursesPage = () => {
-    return <div></div>
-}
+  const [courses, setCourses] = useState([]); // initial value is an empty object
+
+  useEffect(() => {
+    fetch("https://private-e05942-courses22.apiary-mock.com/courses")
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, []);
+  
+  return (
+    <div>
+      <h2>Courses</h2>
+      <ul>
+        {courses.map((course) => (
+          <h1 key={course.id}>
+            {course.title}
+          </h1>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default CoursesPage;
