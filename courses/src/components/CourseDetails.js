dc2feb719edf684c8f3e46c1ae64556bf629853e
Olga <olganedelcuam@gmail.com>
2023-01-22 18:55:03 +0100

    adding the price based on the user's location

--- courses/src/components/CourseDetails.js
+++ courses/src/components/CourseDetails.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const CourseDetails = (props) => {
   const { courseDetails } = props;
+  const [location, setLocation] = useState(null);
+  const [currency, setCurrency] = useState("");
+
+  useEffect(() => {
+    // Fetch the user's location using the ipstack API
+    fetch(
+      "https://api.ipstack.com/check?access_key=d14b88f838f8340e10d91b2d900b8dbb"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setLocation(data);
+        // Determine the appropriate currency to use based on the user's location
+        if (data.country_code === "US") {
+          setCurrency("USD");
+        } else {
+          setCurrency("EUR");
+        }
+      });
+  }, []);
 
   return (
     <>
@@ -14,6 +33,14 @@ const CourseDetails = (props) => {
               <p>{date}</p>
             ))}
           </div>
+          <ul>
+            {courseDetails.prices.map((price) => (
+              <li key={price.id}>
+                {price.amount} {currency}
+              </li>
+            ))}
+          </ul>
+          {/* <p>{courseDetails.prices}</p> */}
         </div>
       )}
     </>
