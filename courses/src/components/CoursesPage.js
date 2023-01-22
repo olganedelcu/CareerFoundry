4e4e1aab5ba7db7206afbb6a895e5b4ac9986b71
Olga <olganedelcuam@gmail.com>
2023-01-22 17:50:27 +0100

    adding a bit of style(similar to CareerFoundry webpage)

--- courses/src/components/CoursesPage.js
+++ courses/src/components/CoursesPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import './CoursesPage.css'
 
 const CoursesPage = () => {
   const [courses, setCourses] = useState([]); // initial value is an empty object
@@ -8,15 +9,15 @@ const CoursesPage = () => {
       .then((res) => res.json())
       .then((data) => setCourses(data));
   }, []);
-  
+
   return (
-    <div>
+    <div className="course-div">
       <h2>Courses</h2>
       <ul>
         {courses.map((course) => (
-          <h1 key={course.id}>
+          <div className="course-title" key={course.id}>
             {course.title}
-          </h1>
+          </div>
         ))}
       </ul>
     </div>
